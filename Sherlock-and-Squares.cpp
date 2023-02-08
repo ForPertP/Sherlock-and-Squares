29886152fc77e1355b512122de3dd009a4a92e81
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-02-09 00:19:56 +0900

    Update Sherlock-and-Squares.cpp

--- Sherlock-and-Squares.cpp
+++ Sherlock-and-Squares.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'squares' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER a
+ *  2. INTEGER b
+ */
+
 int squares(int a, int b)
 {
     int start = std::ceil(std::sqrt(a));
