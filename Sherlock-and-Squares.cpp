567aad6ad965e9c072fc720f5a3617b428e06253
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-02-11 16:30:36 +0900

    Update Sherlock-and-Squares.cpp

--- Sherlock-and-Squares.cpp
+++ Sherlock-and-Squares.cpp
@@ -21,3 +21,32 @@ int squares(int a, int b)
     int end = std::floor(std::sqrt(b));
     return (end - start + 1);
 }
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int a = stoi(first_multiple_input[0]);
+
+        int b = stoi(first_multiple_input[1]);
+
+        int result = squares(a, b);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
